8fc8e2dd157c79c52a44d37f97b0e53a4fcda5ad
Marek Kadlčík <makadlcik@gmail.com>
2016-09-16 19:53:09 +0200

    add DayTime fromString static factory function

--- night-mode.js
+++ night-mode.js
@@ -1,1 +1,30 @@
+function isNumberInOpenInterval(number, min, max) {
+    return number >= min
+        && number <= max;
+}
+var DayTime = (function () {
+    function DayTime(hour, minute) {
+        if (isNumberInOpenInterval(hour, 0, 23)
+            && isNumberInOpenInterval(minute, 0, 59)) {
+            this.hour = hour;
+            this.minute = minute;
+        }
+        else {
+            throw new RangeError("Incorrect time set: " + hour + ":" + minute);
+        }
+    }
+    DayTime.hasValidStructure = function (text) {
+        return /^\d{1,2}:\d{2}$/.test(text);
+    };
+    DayTime.fromString = function (time) {
+        if (DayTime.hasValidStructure(time)) {
+            var splitTime = time.split(":");
+            return new DayTime(Number(splitTime[0]), Number(splitTime[1]));
+        }
+        else {
+            throw new Error("Given value " + time + " didn't match expected format HH:mm");
+        }
+    };
+    return DayTime;
+}());
 //# sourceMappingURL=night-mode.js.map
---
+++ night-mode.ts
@@ -0,0 +1,34 @@
+function isNumberInOpenInterval(number: number, min: number, max: number) {
+    return number >= min
+        && number <= max;
+}
+
+class DayTime {
+
+    hour: number;
+    minute: number;
+
+    private static hasValidStructure(text: string): boolean {
+        return /^\d{1,2}:\d{2}$/.test(text);
+    }
+
+    constructor(hour: number, minute: number) {
+        if (isNumberInOpenInterval(hour, 0, 23)
+            && isNumberInOpenInterval(minute, 0, 59)) {
+            this.hour = hour;
+            this.minute = minute;
+        } else {
+            throw new RangeError("Incorrect time set: " + hour + ":" + minute);
+        }
+    }
+
+    static fromString(time: string): DayTime {
+        if (DayTime.hasValidStructure(time)) {
+            let splitTime = time.split(":");
+            return new DayTime(Number(splitTime[0]), Number(splitTime[1]));
+        } else {
+            throw new Error("Given value " + time + " didn't match expected format HH:mm");
+        }
+    }
+
+}
